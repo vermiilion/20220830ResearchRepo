8ce6fb23087a64bad3354d79d7935f9ae70aa66f
vermiilion <110362046+vermiilion@users.noreply.github.com>
2022-08-21 21:36:39 +1200

    Create days-per-month.py

---
+++ Research/Python/days-per-month.py
@@ -0,0 +1,12 @@
+months_with_31days = ["January", "March", "May", "July", "August", "October", "December"]
+months_with_30days = ["April", "June", "September", "November"]
+months_with_28_29days = ["February"]
+
+input_from_user = input("Input the name of Month: ")
+
+if input_from_user in months_with_31days:
+   print("No. of days: 31 days")
+elif input_from_user in months_with_30days:
+   print("No. of days: 30 days")
+elif input_from_user in months_with_28_29days:
+   print("No. of days: 28/29 days")
