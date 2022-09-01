86cf2a82231312b327f24b24e8f293c778861ec3
vermiilion <110362046+vermiilion@users.noreply.github.com>
2022-09-01 23:26:11 +1200

    Update dog-years-calculator.py

--- Software Development/Code/Python/Python Exercises/dog-years-calculator.py
+++ Software Development/Code/Python/Python Exercises/dog-years-calculator.py
@@ -1,3 +1,6 @@
+# Activity 1 Calculate a dog’s age in dog years.
+#Note: For the first two years, a dog year is equal to 10.5 human years. After that, each dog year equals 4 human years. 
+
 dog_age = int(input("Input dog's age in human years: "))
 
 if dog_age <= 2:
